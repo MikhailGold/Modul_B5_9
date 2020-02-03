167a1c939a8dd72e94f1b50c38960649ee3a8274
MikhailGold <58542664+MikhailGold@users.noreply.github.com>
2020-02-03 15:44:22 +0600

    Add files via upload

--- decorators.py
+++ decorators.py
@@ -1,7 +1,9 @@
 import time
 
+
 def benchmark(func):
     import time
+
     def wrapper(*args, **kwargs):
         start = time.time()
         return_value = func(*args, **kwargs)
@@ -9,14 +11,15 @@ def benchmark(func):
         print('[*] Время выполнения: {:.3f} секунд.'.format(end-start))
         return return_value
     return wrapper
-    
+
 
 def benchmark_par(iters):
     def actual_decorator(func):
         import time
+
         def wrapper(*args, **kwargs):
             total = 0
-            for i in range(1,iters+1):
+            for i in range(1, iters+1):
                 start = time.time()
                 return_value = func(*args, **kwargs)
                 end = time.time()
@@ -34,6 +37,7 @@ def fetch_webpage(url):
     webpage = requests.get(url)
     return webpage.text
 
+
 @benchmark_par(3)
 def fetch_webpage_1(url):
     import requests
@@ -41,13 +45,36 @@ def fetch_webpage_1(url):
     return webpage.text
 
 
-class benchmark_class(object):
-    def start(self):
-        self.start = time.time()
+class benchmark_class:
+    def __init__(self, iters=10):
+        self.iters = iters
+        self.avg_time = 0
+        self.t0 = 0
+        self.t1 = 0
+
+    def __call__(self, func):
+        def wrap(*args, **kwargs):
+            avg_t = 0
+            for i in range(self.iters):
+                t0 = time.time()
+                func(*args, **kwargs)
+                t1 = time.time()
+                avg_t += (t1 - t0)
+
+            self.avg_time = avg_t/self.iters
+            print("[*]Среднее время выполнения 1 запуска из",
+                  self.iters, "заняло %.5f секунд" % self.avg_time)
+        return wrap
+
+    def __enter__(self):
+        self.t0 = time.time()
         return self
-    def end(self, *args, **kwargs):
-        self.end = time.time()
-        print('[*] Время выполнения: {:.3f} секунд.'.format(self.end-self.start))
+
+    def __exit__(self, type, value, traceback):
+        self.t1 = time.time()
+        print("[*]Время выполнения 1 запуска заняло %.5f секунд" %
+              (self.t1 - self.t0))
+        return self
 
 print("Время до www.psu.by")
 webpage=fetch_webpage("https://www.psu.by")
@@ -61,8 +88,20 @@ webpage = fetch_webpage_1("https://www.psu.by")
 print("Время до www.nur.kz")
 webpage = fetch_webpage_1("https://www.nur.kz")
 
+
 print("Декоратор класс - секундомер")
-a = benchmark_class()
-a.start()
-time.sleep(5)
-a.end()
+@benchmark_class(iters=3)
+def pause():
+    import time
+    time.sleep(1)
+
+pause()
+
+
+def pause_2():
+    import time
+    time.sleep(2)
+
+
+with benchmark_class() as tf:
+    pause_2()
